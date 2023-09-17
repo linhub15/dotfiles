69509e9734bfbd41e49b66b802a8d13f6a066d08
Hubert Lin <linhub15@gmail.com>
2023-09-16 18:30:17 -0600

    git: add default configs

---
+++ git/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config --global user.name "Hubert Lin"
+git config --global user.email "linhub15@gmail.com"
+
+git config --global alias.l "log --color --graph --pretty=format:'%Cred%h%Creset%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+git config --global init.defaultBranch main
