7db8bb9080f6ccadc83b86d571e914eec77d87cd
Hubert Lin <linhub15@gmail.com>
2023-09-25 20:12:02 -0600

    add lsp settings

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -4,6 +4,6 @@ vim.opt.relativenumber = true
 vim.g.mapleader = " "
 
 vim.keymap.set("i", "jj", "<Esc>")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" } )
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" } )
 
 vim.keymap.set("n", "<leader>x", vim.cmd.Lazy, { desc = "Lazy plugin manager" })
