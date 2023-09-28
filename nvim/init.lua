01448f7a46119534a043c0ecfc7ec50ecde1ee3c
Hubert Lin <linhub15@gmail.com>
2023-09-27 20:42:04 -0600

    nvim: float the file explorer

--- nvim/init.lua
+++ nvim/init.lua
@@ -83,8 +83,44 @@ local function nvim_tree_on_attach(bufnr)
 	-- custom mappings
 	vim.keymap.set('n', '<leader>E', api.tree.toggle, { desc = "[E]xplorer" })
 end
+
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.5
 require("nvim-tree").setup({
-	on_attach = nvim_tree_on_attach
+	disable_netrw = true,
+	hijack_netrw = true,
+	respect_buf_cwd = true,
+	sync_root_with_cwd = true,
+	on_attach = nvim_tree_on_attach,
+	view = {
+		relativenumber = true,
+		float = {
+			enable = true,
+			open_win_config = function()
+				local screen_w = vim.opt.columns:get()
+				local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+				local window_w = screen_w * WIDTH_RATIO
+				local window_h = screen_h * HEIGHT_RATIO
+				local window_w_int = math.floor(window_w)
+				local window_h_int = math.floor(window_h)
+				local center_x = (screen_w - window_w) / 2
+				local center_y = ((vim.opt.lines:get() - window_h) / 2)
+				    - vim.opt.cmdheight:get()
+				return {
+					border = "rounded",
+					relative = "editor",
+					row = center_y,
+					col = center_x,
+					width = window_w_int,
+					height = window_h_int,
+				}
+			end,
+		},
+		width = function()
+			return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+		end,
+	},
+
 })
 
 
@@ -119,7 +155,7 @@ local lspconfig_on_attach = function(_, bufnr)
 
 	-- See `:help K` for why this keymap
 	nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentatimn')
 
 	-- Lesser used LSP functionality
 	nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
