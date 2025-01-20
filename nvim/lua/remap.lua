92d624be7feee5e5d9b90343f8946f23a82a7ac9
Hubert Lin <linhub15@users.noreply.github.com>
2025-01-19 23:29:15 -0700

    remove nvim configs

--- nvim/lua/remap.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.g.mapleader = " "
-
-vim.keymap.set("i", "jj", "<Esc>")
--- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" } )
-
-vim.keymap.set("n", "<C-X>", vim.cmd.Lazy, { desc = "Lazy plugin manager" })
